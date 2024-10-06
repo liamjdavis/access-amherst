b4d6381ec29816be569e666b8a3a7a69508dfecf
Tairan 'Ryan' Ji <tairanjiryan@gmail.com>
2024-10-05 23:58:55 -0400

    Finalized Django backend framework

--- access_amherst_backend/access_amherst_backend/urls.py
+++ access_amherst_backend/access_amherst_backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from access_amherst_algo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("access_amherst_algo.urls")),
 ]
