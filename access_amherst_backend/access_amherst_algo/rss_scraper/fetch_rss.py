36d378d62a2f6b025da524bc43784cb99113e34c
Tairan 'Ryan' Ji <tairanjiryan@gmail.com>
2024-10-13 14:40:37 -0400

    Created the RSS fetcher and parser

---
+++ access_amherst_backend/access_amherst_algo/rss_scraper/fetch_rss.py
@@ -0,0 +1,10 @@
+import requests
+from datetime import datetime
+
+url = 'https://thehub.amherst.edu/events.rss'
+response = requests.get(url)
+
+file_name = 'access_amherst_backend/access_amherst_algo/rss_scraper/rss_files/hub_' + datetime.now().strftime('%Y_%m_%d_%H') + '.xml'
+# Save the content as an XML file
+with open(file_name, 'wb') as file:
+    file.write(response.content)
